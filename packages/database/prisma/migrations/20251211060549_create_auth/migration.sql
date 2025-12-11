6bfacf97db8c46e025699b6ce203a5daeb6d6ea0
turisanapo <selvaggiodavide@hotmail.it>
2025-12-11 16:35:30 +0800

    fix(auth): redo auth migrations

--- packages/database/prisma/migrations/20251211060549_create_auth/migration.sql
+++ packages/database/prisma/migrations/20251211060549_create_auth/migration.sql
@@ -59,6 +59,33 @@ CREATE TABLE "auth"."verifications" (
     CONSTRAINT "verifications_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "auth"."apikeys" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "start" TEXT,
+    "prefix" TEXT,
+    "key" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "refillInterval" INTEGER,
+    "refillAmount" INTEGER,
+    "lastRefillAt" TIMESTAMP(3),
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "rateLimitEnabled" BOOLEAN NOT NULL DEFAULT true,
+    "rateLimitTimeWindow" INTEGER,
+    "rateLimitMax" INTEGER,
+    "requestCount" INTEGER NOT NULL DEFAULT 0,
+    "remaining" INTEGER,
+    "lastRequest" TIMESTAMP(3),
+    "expiresAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "permissions" TEXT,
+    "metadata" JSONB,
+
+    CONSTRAINT "apikeys_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "auth"."users"("email");
 
@@ -74,8 +101,14 @@ CREATE INDEX "accounts_userId_idx" ON "auth"."accounts"("userId");
 -- CreateIndex
 CREATE UNIQUE INDEX "accounts_providerId_accountId_key" ON "auth"."accounts"("providerId", "accountId");
 
+-- CreateIndex
+CREATE INDEX "apikeys_userId_idx" ON "auth"."apikeys"("userId");
+
 -- AddForeignKey
 ALTER TABLE "auth"."sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "auth"."accounts" ADD CONSTRAINT "accounts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth"."apikeys" ADD CONSTRAINT "apikeys_userId_fkey" FOREIGN KEY ("userId") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
