4df31ae115f322d701b323391d89be5b4e3d7006
turisanapo <selvaggiodavide@hotmail.it>
2025-12-12 12:37:20 +0800

    fix(database): migrator

--- packages/database/lambda/migrator.ts
+++ packages/database/lambda/migrator.ts
@@ -1,9 +1,17 @@
 import { exec } from "node:child_process";
 import { promisify } from "node:util";
 
+import { getConnectionString } from "../src/connection";
+
 export const handler = async () => {
   await promisify(exec)(
     "npx prisma migrate deploy --config ./prisma.config.ts",
+    {
+      env: {
+        ...process.env,
+        DATABASE_URL: getConnectionString(),
+      },
+    },
   );
   return { ok: true };
 };
