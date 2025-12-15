b98515bf7578603405af1662af0e197a8d41f90a
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-12-15 20:48:33 +0800

    fix(www): mobile style tweaks

--- apps/www/app/footer.tsx
+++ apps/www/app/footer.tsx
@@ -3,22 +3,19 @@ import { Link } from "react-router";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col gap-10">
+    <footer className="mb-5 flex flex-col gap-10">
       <section className="border-border mx-auto max-w-5xl rounded-xl border bg-slate-100 p-6 ">
-        <div className="relative grid gap-8 sm:grid-cols-[1.5fr_1fr] sm:items-center">
-          <div className="flex items-center gap-4">
-            <img
-              src="/hebo.png"
-              alt="Hebo Cloud"
-              className="h-20 w-20 sm:h-32 sm:w-32"
-            />
+        <div className="relative grid gap-8 sm:grid-cols-[1.3fr_1fr] sm:items-center">
+          <div className="flex flex-col items-center gap-4 text-center sm:flex-row sm:items-center sm:text-left">
+            <img src="/hebo.png" alt="Hebo Cloud" className="h-32 w-32" />
             <div className="flex flex-col gap-2">
               <h2 className="text-2xl font-semibold sm:text-3xl">Hebo Cloud</h2>
               <p className="text-muted-foreground">
-                Hebo is the fastest way to build and scale high-quality
-                conversational agents.
+                Hebo is the fastest way to build and scale{" "}
+                <span className="text-nowrap">high-quality</span> conversational
+                agents.
               </p>
-              <div className="flex flex-wrap items-center gap-3">
+              <div className="flex flex-wrap items-center justify-center gap-3 sm:justify-start">
                 <a
                   href="https://console.hebo.ai"
                   target="_blank"
@@ -48,9 +45,9 @@ export default function Footer() {
               <div className="flex gap-2 rounded-xl bg-white/70 p-4 hover:bg-white sm:flex-col">
                 <Brain className="size-10" />
                 <div>
-                  <div className="text-sm font-semibold">Gateway &gt;</div>
+                  <div className="font-semibold sm:text-sm">Gateway &gt;</div>
                   <div className="text-muted-foreground text-xs">
-                    Instant access to SOTA models
+                    Instant access to SOTA LLM models
                   </div>
                 </div>
               </div>
@@ -59,7 +56,7 @@ export default function Footer() {
               <div className="flex gap-2 rounded-xl bg-white/70 p-4 hover:bg-white sm:flex-col">
                 <SquareFunction className="size-10" />
                 <div>
-                  <div className="text-sm font-semibold">MCP &gt;</div>
+                  <div className="font-semibold sm:text-sm">MCP &gt;</div>
                   <div className="text-muted-foreground text-xs">
                     Ready-to-use tools via our hosted MCP
                   </div>
@@ -70,7 +67,7 @@ export default function Footer() {
               <div className="flex gap-2 rounded-xl bg-white/70 p-4 hover:bg-white sm:flex-col">
                 <Gauge className="size-10" />
                 <div>
-                  <div className="text-sm font-semibold">Evals &gt;</div>
+                  <div className="font-semibold sm:text-sm">Evals &gt;</div>
                   <div className="text-muted-foreground text-xs">
                     Human-first evals, written in Markdown
                   </div>
@@ -82,12 +79,12 @@ export default function Footer() {
       </section>
 
       <div className="flex flex-wrap justify-center gap-2 text-xs sm:text-sm">
-        <div className="flex items-center gap-1 whitespace-nowrap">
+        <div className="flex items-center gap-1">
           <img src="/icon.png" alt="Hebo Logo" className="size-4" />
           <span className="font-semibold"> hebo.ai</span>
           <span>is designed, built and backed by</span>
         </div>
-        <div className="flex items-center gap-1 whitespace-nowrap">
+        <div className="flex items-center gap-1">
           <img
             src="https://8monkey.ai/icon.png"
             alt="8monkey Logo"
