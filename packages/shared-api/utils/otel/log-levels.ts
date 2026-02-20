0d414f4b6549095c716132fd1cf27ab3fd4a7e41
turisanapo <selvaggiodavide@hotmail.it>
2026-02-20 10:22:19 +0800

    chores(otel): remove unnecessary type

--- packages/shared-api/utils/otel/log-levels.ts
+++ packages/shared-api/utils/otel/log-levels.ts
@@ -8,10 +8,7 @@ export const otelLogLevels = [
   "error",
 ] as const;
 
-export const otelSeverityByLevel: Record<
-  (typeof otelLogLevels)[number],
-  SeverityNumber
-> = {
+export const otelSeverityByLevel = {
   trace: SeverityNumber.TRACE,
   debug: SeverityNumber.DEBUG,
   info: SeverityNumber.INFO,
