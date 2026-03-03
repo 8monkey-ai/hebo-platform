de6b516d7f202715c4e622e3e335a20611657e40
Heinrich Wendel <heinrich.wendel@gmail.com>
2026-03-03 21:42:49 +0800

    fix: sidebar

--- packages/shared-ui/src/components/Sidebar.tsx
+++ packages/shared-ui/src/components/Sidebar.tsx
@@ -1,119 +1,10 @@
 import { PanelLeftIcon } from "lucide-react";
-import React from "react";
 
-import {
-  useSidebar,
-  Sidebar as ShadCnSidebar,
-  SidebarContext,
-  SidebarContextProps,
-} from "#/_shadcn/ui/sidebar";
-import { useIsMobile } from "#/hooks/use-mobile";
+import { Button } from "#/_shadcn/ui/button";
+import { useSidebar } from "#/_shadcn/ui/sidebar";
 import { cn } from "#/lib/utils";
 
-import { Button } from "./Button";
-
-const SIDEBAR_WIDTH_ICON = "3rem";
-const SIDEBAR_WIDTH = "16rem";
-const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-
-function SidebarProvider({
-  defaultOpen = true,
-  open: openProp,
-  onOpenChange: setOpenProp,
-  className,
-  style,
-  children,
-  shortcut = "b",
-  cookieName = "sidebar_state",
-  ...props
-}: React.ComponentProps<"div"> & {
-  defaultOpen?: boolean;
-  open?: boolean;
-  shortcut?: string;
-  cookieName?: string;
-  onOpenChange?: (open: boolean) => void;
-}) {
-  const isMobile = useIsMobile();
-  const [openMobile, setOpenMobile] = React.useState(false);
-
-  // This is the internal state of the sidebar.
-  // We use openProp and setOpenProp for control from outside the component.
-  const [_open, _setOpen] = React.useState(defaultOpen);
-  const open = openProp ?? _open;
-  const setOpen = React.useCallback(
-    (value: boolean | ((value: boolean) => boolean)) => {
-      const openState = typeof value === "function" ? value(open) : value;
-      if (setOpenProp) {
-        setOpenProp(openState);
-      } else {
-        _setOpen(openState);
-      }
-
-      // This sets the cookie to keep the sidebar state.
-      document.cookie = `${cookieName}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`;
-    },
-    [setOpenProp, open, cookieName],
-  );
-
-  // Helper to toggle the sidebar.
-  const toggleSidebar = React.useCallback(() => {
-    return isMobile ? setOpenMobile((open) => !open) : setOpen((open) => !open);
-  }, [isMobile, setOpen, setOpenMobile]);
-
-  // Adds a keyboard shortcut to toggle the sidebar.
-  React.useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === shortcut && (event.metaKey || event.ctrlKey)) {
-        event.preventDefault();
-        toggleSidebar();
-      }
-    };
-
-    globalThis.addEventListener("keydown", handleKeyDown);
-    return () => globalThis.removeEventListener("keydown", handleKeyDown);
-  }, [shortcut, toggleSidebar]);
-
-  // We add a state so that we can do data-state="expanded" or "collapsed".
-  // This makes it easier to style the sidebar with Tailwind classes.
-  const state = open ? "expanded" : "collapsed";
-
-  const contextValue = React.useMemo<SidebarContextProps>(
-    () => ({
-      state,
-      open,
-      setOpen,
-      isMobile,
-      openMobile,
-      setOpenMobile,
-      toggleSidebar,
-    }),
-    [state, open, setOpen, isMobile, openMobile, setOpenMobile, toggleSidebar],
-  );
-
-  return (
-    <SidebarContext.Provider value={contextValue}>
-      <div
-        data-slot="sidebar-wrapper"
-        style={
-          {
-            "--sidebar-width": SIDEBAR_WIDTH,
-            "--sidebar-width-icon": SIDEBAR_WIDTH_ICON,
-            ...style,
-          } as React.CSSProperties
-        }
-        className={cn(
-          "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
-          className,
-        )}
-        {...props}
-      >
-        {children}
-      </div>
-    </SidebarContext.Provider>
-  );
-}
-
-function SidebarTrigger({
+export function SidebarTrigger({
   className,
   onClick,
   icon = <PanelLeftIcon />,
@@ -142,38 +33,8 @@ function SidebarTrigger({
   );
 }
 
-function Sidebar({
-  side = "left",
-  variant = "sidebar",
-  collapsible = "offcanvas",
-  className,
-  children,
-  ...props
-}: React.ComponentProps<"div"> & {
-  side?: "left" | "right";
-  variant?: "sidebar" | "floating" | "inset";
-  collapsible?: "offcanvas" | "icon" | "none";
-}) {
-  const { open } = useSidebar();
-
-  return (
-    <ShadCnSidebar
-      side={side}
-      variant={variant}
-      collapsible={collapsible}
-      className={className}
-      inert={collapsible === "offcanvas" && !open ? true : undefined}
-      {...props}
-    >
-      {children}
-    </ShadCnSidebar>
-  );
-}
-
-export { SidebarProvider, Sidebar, SidebarTrigger };
-
 export {
-  useSidebar,
+  Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
@@ -192,7 +53,8 @@ export {
   SidebarMenuSub,
   SidebarMenuSubButton,
   SidebarMenuSubItem,
+  SidebarProvider,
   SidebarRail,
   SidebarSeparator,
-  SidebarContext,
+  useSidebar,
 } from "#/_shadcn/ui/sidebar";
