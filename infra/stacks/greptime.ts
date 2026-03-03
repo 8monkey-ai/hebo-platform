bbfd97731d3b2ab0eaf99ad5134509f95c8a123c
turisanapo <selvaggiodavide@hotmail.it>
2026-03-03 10:22:51 +0800

    chores(otel): keep port binding

--- infra/stacks/greptime.ts
+++ infra/stacks/greptime.ts
@@ -12,10 +12,14 @@ const heboGreptime = isProduction
       cpu: "0.25 vCPU",
       memory: "0.5 GB",
       image: "greptime/greptimedb:v1.0.0-rc.1",
-      command: ["standalone", "start", "--http-addr=0.0.0.0:4000"],
-      serviceRegistry: {
-        port: 4000,
-      },
+      command: [
+        "standalone",
+        "start",
+        "--http-addr=0.0.0.0:4000",
+        "--rpc-bind-addr=0.0.0.0:4001",
+        "--mysql-addr=0.0.0.0:4002",
+        "--postgres-addr=0.0.0.0:4003",
+      ],
       scaling: {
         min: 1,
         max: 1,
