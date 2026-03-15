c6f82fddd4b4f91f7560f787d47c6586d12fc6ce
turisanapo <selvaggiodavide@hotmail.it>
2026-03-15 19:21:16 +0800

    fix(db): pin migrator version

--- packages/shared-api/lib/db/lambda/migrator.ts
+++ packages/shared-api/lib/db/lambda/migrator.ts
@@ -4,7 +4,7 @@ import { promisify } from "node:util";
 import { getConnectionString } from "../connection";
 
 export const handler = async (event: { schema: string }) => {
-  await promisify(exec)("npx prisma migrate deploy --config ./prisma.config.ts", {
+  await promisify(exec)("npx prisma@7.4.2 migrate deploy --config ./prisma.config.ts", {
     env: {
       ...process.env,
       DATABASE_URL: getConnectionString(event.schema),
