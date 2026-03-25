d42fa44484a7559e288dd22dc06484a8ef7c3ac2
Heinrich Wendel <heinrich.wendel@gmail.com>
2026-03-25 10:15:27 +0800

    fix: types

--- packages/shared-api/utils/secrets.ts
+++ packages/shared-api/utils/secrets.ts
@@ -1,11 +1,11 @@
 import { secrets } from "bun";
 import { Resource } from "sst";
 
-export const getSecret = async (name: string) => {
+export const getSecret = async (name: string): Promise<string | null> => {
   try {
     // @ts-expect-error: Resource may not be defined
     const value = Resource[name].value;
-    return value === "undefined" ? undefined : value;
+    return value === "undefined" ? null : value;
   } catch {
     return secrets.get({ service: "hebo", name });
   }
