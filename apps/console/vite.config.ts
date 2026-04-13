6e80190bae0e2e061a96cfb838dea121d550007b
turisanapo <selvaggiodavide@hotmail.it>
2026-04-13 13:29:55 +0800

    refactor: update Vite configuration to use a function for dynamic settings and enable devtools JSON file

--- apps/console/vite.config.ts
+++ apps/console/vite.config.ts
@@ -6,38 +6,41 @@ import preserveDirectives from "rollup-preserve-directives";
 import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
 
-export default defineConfig({
-  resolve: { tsconfigPaths: true },
-  server: { port: 8520 },
-  optimizeDeps: {
-    entries: ["app/root.tsx", "app/routes/**/route.{ts,tsx,mdx}"],
-    include: ["react/compiler-runtime"],
-  },
-  plugins: [
-    preserveDirectives(),
-    mdx({
-      providerImportSource: "~console/mdx-components",
-      rehypePlugins: [
-        [
-          rehypeShiki,
-          {
-            theme: "vitesse-light",
-            langs: ["bash", "python", "ts"],
-            addLanguageClass: true,
-            parseMetaString: (meta: string): Record<string, string> =>
-              meta ? { title: meta.trim() } : {},
-          },
+export default defineConfig(() => {
+  return {
+    resolve: { tsconfigPaths: true },
+    server: { port: 8520, devtoolsJsonFile: true },
+    optimizeDeps: {
+      entries: ["app/root.tsx", "app/routes/**/route.{ts,tsx,mdx}"],
+      include: ["react/compiler-runtime"],
+    },
+    plugins: [
+      preserveDirectives(),
+      mdx({
+        providerImportSource: "~console/mdx-components",
+        rehypePlugins: [
+          [
+            rehypeShiki,
+            {
+              theme: "vitesse-light",
+              langs: ["bash", "python", "ts"],
+              // Add code block metadata as HTML attributes
+              addLanguageClass: true,
+              parseMetaString: (meta: string): Record<string, string> =>
+                meta ? { title: meta.trim() } : {},
+            },
+          ],
         ],
-      ],
-    }),
-    tailwindcss(),
-    reactRouter(),
-    babel({
-      filter: (id) => /\.[jt]sx?$/.test(id) && !id.includes("node_modules"),
-      babelConfig: {
-        presets: ["@babel/preset-typescript"],
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+      }),
+      tailwindcss(),
+      reactRouter(),
+      babel({
+        filter: (id) => /\.[jt]sx?$/.test(id) && !id.includes("node_modules"),
+        babelConfig: {
+          presets: ["@babel/preset-typescript"],
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+  };
 });
